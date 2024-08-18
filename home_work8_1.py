e67e5e7d6fab420c014739c52272a2b2371a700b
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-08-18 14:07:16 +0300

    Add files via upload

---
+++ home_work8_1.py
@@ -0,0 +1,15 @@
+def add_everything_up(a, b):
+    if type(a) == float and isinstance(b, (int, float)):
+        x = len(str(a).split('.')[1])
+        a = a * x * 10 / x / 10
+    elif type(b) == float and isinstance(a, (int, float)):
+        y = len(str(b).split('.')[1])
+        b = b * y * 10 / y / 10
+    try:
+        return a + b
+    except(TypeError):
+        return str(a) + str(b)
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
